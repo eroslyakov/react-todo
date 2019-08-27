0018f16a15a1fdf8364b187b4df5d5566dbcd5f2
eroslyakov <ya.stinson@yandex.ru>
2019-08-27 22:27:34 +0300

    some cleanups

--- src/index.js
+++ src/index.js
@@ -4,30 +4,3 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// class App extends Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             monsters: []
-//         };
-//     }
-
-//     componentDidMount() {
-//         fetch("https://pokeapi.co/api/v2/pokemon")
-//         .then(response => response.json())
-//         .then(json => this.setState({ monsters: json.results }));
-//     }
-
-//     render() {
-//         return (
-//             <div className='app'>
-//                 <h1>Welcome</h1>
-//             {
-//                 this.state.monsters.map(monster => <h2 key={monster.url}>{monster.name}</h2>)
-//             }
-//             </div>
-//         )
-//     }
-// }
-
